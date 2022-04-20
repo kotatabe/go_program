295bf2dbdf86183d1adaabcf00b2c077dfe333f6
kotatabe <qwerty789kota@gmail.com>
2022-04-20 16:58:46 +0900

    OutputFile()を別ファイルに分けた

--- pg02/main.go
+++ pg02/main.go
@@ -6,21 +6,6 @@ import (
 	"io"
 )
 
-func OutputFile(filename string) {
-	fp, err := os.Open(filename)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "File Open failed")
-		return
-	}
-	defer fp.Close()
-
-	_, err = io.Copy(os.Stdout, fp)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "File Copy failed")
-		return
-	}
-}
-
 func main() {
 	if len(os.Args) > 2 || len(os.Args) <= 1 {
 		fmt.Fprintln(os.Stderr, "Invalid argument...")
